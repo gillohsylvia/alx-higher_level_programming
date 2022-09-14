1992dd21b244430201e8466796223a22c889b464
gillohsylvia <abdallahfatima363@gmail.com>
2022-09-14 18:13:35 +0000

    Updating task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""docstring for Square"""
+"""class Square that defines a square"""
 
 
-class Square:
-    """docstring for init"""
+class Square():
+    """square class with it's size and proper validation"""
     def __init__(self, size=0):
-        if isinstance(size, int) and size >= 0:
-            self.__size = size
-        elif not isinstance(size, int):
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif (size < 0):
             raise ValueError("size must be >= 0")
+        self.__size = size
