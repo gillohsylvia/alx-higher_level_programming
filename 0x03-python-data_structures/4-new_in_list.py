f9b4f85f34d4a80fda7aadce56641068736a0abc
gillohsylvia <abdallahfatima363@gmail.com>
2022-10-04 03:14:22 -0700

    Updating files

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    tmp_list = my_list[:]
-    if 0 <= idx < len(my_list):
-        tmp_list[idx] = element
-        return(tmp_list)
-    return(my_list)
+    new_list = my_list.copy()
+    if idx < 0:
+        return new_list
+    elif idx > len(new_list) - 1:
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return new_string
+    if my_string is not None:
+        new_string = ''
+        for i in my_string:
+            if (i != 'c') and (i != 'C'):
+                new_string += i
+        return new_string
