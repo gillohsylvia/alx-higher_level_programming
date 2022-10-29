efe45f1b15cb89806152cc4991fd21d6e62af5b3
gillohsylvia <abdallahfatima363@gmail.com>
2022-10-28 22:52:41 -0700

    Correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+    newList = [[x**2 for x in y]for y in matrix]
+    return newList
